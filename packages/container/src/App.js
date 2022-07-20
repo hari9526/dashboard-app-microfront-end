b1d90e6d705312a47266ce4e6f60379602afd2e5
Harikrishnan R <hari04.krishnan@gmail.com>
2022-07-21 01:28:25 +0530

    Bug fix: Header styling issue.

--- packages/container/src/App.js
+++ packages/container/src/App.js
@@ -2,14 +2,28 @@ import React from "react";
 import { BrowserRouter } from "react-router-dom";
 import Header from "./components/Header";
 import MarketingApp from "./components/MarketingApp";
+import {
+  StylesProvider,
+  createGenerateClassName,
+} from "@material-ui/core/styles";
+
+//Instead of creating classnames with jss, like jss1, jss2,
+//we are generating it as ma1, ma2 for the production site
+//so that we can avoid the name collision.
+//This won't have an impact on that mfes.
+const generateClassName = createGenerateClassName({
+  productionPrefix: "co",
+});
 
 export default () => {
   return (
     <BrowserRouter>
-      <div>
-        <Header />
-        <MarketingApp />
-      </div>
+      <StylesProvider generateClassName={generateClassName}>
+        <div>
+          <Header />
+          <MarketingApp />
+        </div>
+      </StylesProvider>
     </BrowserRouter>
   );
 };
--- packages/marketing/src/App.js
+++ packages/marketing/src/App.js
@@ -1,14 +1,23 @@
 import React from "react";
 import { Switch, BrowserRouter, Route } from "react-router-dom";
-import { StylesProvider } from "@material-ui/core/styles";
+import { StylesProvider, createGenerateClassName} from "@material-ui/core/styles";
 
 import Landing from "./components/Landing";
 import Pricing from "./components/Pricing";
 
+
+//Instead of creating classnames with jss, like jss1, jss2, 
+//we are generating it as ma1, ma2 for the production site 
+//so that we can avoid the name collision. 
+//This won't have an impact on that mfes. 
+const generateClassName = createGenerateClassName({
+  productionPrefix: 'ma', 
+}); 
+
 export default () => {
   return (
     <div>
-      <StylesProvider>
+      <StylesProvider generateClassName={generateClassName}>
         <BrowserRouter>
           <Switch>
             <Route exact path='/pricing' component={Pricing}/>
